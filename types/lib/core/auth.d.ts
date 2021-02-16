3812f415e9c738ade6f7f19c8b18f2196eda74b1
Neljon Lusterio <neljon@elephantventures.com>
2021-02-16 23:10:00 +0800

    Updated base compliance-workflow.js

--- types/lib/core/auth.d.ts
+++ types/lib/core/auth.d.ts
@@ -1,9 +1,9 @@
 export = Auth;
 declare class Auth {
     constructor(programUid: any, hmac: any, api: any, tokenMaxAge?: number);
-    /** @protected */ protected _programUid: any;
-    /** @protected */ protected _hmac: any;
-    /** @protected */ protected _api: any;
-    /** @protected */ protected _tokenMaxAge: number;
+    /** @ignore @protected */ protected _programUid: any;
+    /** @ignore @protected */ protected _hmac: any;
+    /** @ignore @protected */ protected _api: any;
+    /** @ignore @protected */ protected _tokenMaxAge: number;
     getToken(): Promise<any>;
 }
--- types/lib/core/compliance-workflow.d.ts
+++ types/lib/core/compliance-workflow.d.ts
@@ -1,11 +1,24 @@
 export = ComplianceWorkflow;
+/**
+ * The Compliance Workflow resource class
+ */
 declare class ComplianceWorkflow {
+    /** @hideconstructor */
     constructor(api: any, auth: any);
-    /** @protected */ protected _api: any;
-    /** @protected */ protected _auth: any;
+    /** @ignore @protected */ protected _api: any;
+    /** @ignore @protected */ protected _auth: any;
+    /**
+     * Associate a new Compliance Workflow and set of Compliance Documents (for acknowledgment) with a Customer.
+     * @param {string} customerExternalUid
+     * @param {string} email
+     * @returns {Promise<any>}
+     */
+    create(customerExternalUid: string, email: string): Promise<any>;
+    renew(customerExternalUid: any, email: any, customerUid: any): void;
     /**
      * Retrieves the most recent Compliance Workflow for a Customer. A Customer UID must be supplied as the path parameter.
      * @returns {Promise<string>}
      */
-    viewLatest(): Promise<string>;
+    viewLatest(customerUid: any): Promise<string>;
+    acknowledgeComplianceDocument(workflowUid: any, documentUid: any): void;
 }
