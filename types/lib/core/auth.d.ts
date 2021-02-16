bd73d881321f8ea792499d88dba097a50e6004bf
Neljon Lusterio <neljon@elephantventures.com>
2021-02-16 22:35:07 +0800

    Inline access modifiers

--- types/lib/core/auth.d.ts
+++ types/lib/core/auth.d.ts
@@ -1,13 +1,9 @@
 export = Auth;
 declare class Auth {
     constructor(programUid: any, hmac: any, api: any, tokenMaxAge?: number);
-    /** @protected */
-    protected _programUid: any;
-    /** @protected */
-    protected _hmac: any;
-    /** @protected */
-    protected _api: any;
-    /** @protected */
-    protected _tokenMaxAge: number;
+    /** @protected */ protected _programUid: any;
+    /** @protected */ protected _hmac: any;
+    /** @protected */ protected _api: any;
+    /** @protected */ protected _tokenMaxAge: number;
     getToken(): Promise<any>;
 }
