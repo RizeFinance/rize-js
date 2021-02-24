7fc37631465f655ca26929e8a740aa2252f50969
doydoydoy <colourd.eyes@gmail.com>
2021-02-24 18:07:29 +0800

    Update synthetic-account.js, synthetic-account.typedefs.js, and synthetic-account.spec.js

--- test/core/synthetic-account.spec.js
+++ test/core/synthetic-account.spec.js
@@ -1,1 +1,20 @@
-//
+'use strict';
+
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+
+chai.use(chaiAsPromised);
+const expect = chai.expect;
+
+const Rize = require('../../index');
+const rizeClient = new Rize(
+    process.env.RIZE_PROGRAM_ID,
+    process.env.RIZE_HMAC
+);
+
+describe('Synthetic Account', () => {
+    it('Test', async () => {
+        const test = await rizeClient.syntheticAccount.getList();
+        expect(test).to.not.be.empty;
+    });
+});
