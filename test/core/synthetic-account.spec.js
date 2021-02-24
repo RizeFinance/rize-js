b21ba0d7ae90aeb1883ed0706a139cd46dc437e1
Neljon Lusterio <neljon@elephantventures.com>
2021-02-24 20:18:28 +0800

    Added tests for syntheticAccount.getTypesList

--- test/core/synthetic-account.spec.js
+++ test/core/synthetic-account.spec.js
@@ -13,8 +13,37 @@ const rizeClient = new Rize(
 );
 
 describe('Synthetic Account', () => {
-    it('Test', async () => {
-        const test = await rizeClient.syntheticAccount.getList();
-        expect(test).to.not.be.empty;
+    const verifySyntheticAccountTypesList = (list, limit, offset) => {
+        expect(list).to.have.property('total_count').to.be.a('number');
+        expect(list).to.have.property('count').to.be.a('number');
+        expect(list).to.have.property('limit').to.be.a('number').that.equals(limit);
+        expect(list).to.have.property('offset').to.be.a('number').that.equals(offset);
+        expect(list).to.have.property('data').to.be.an('array');
+    };
+
+    describe('getTypesList', () => {
+        it('Throws an error if "query" is invalid', () => {
+            const promise = rizeClient.syntheticAccount.getTypesList('');
+            return expect(promise).to.eventually.be.rejectedWith('"query" must be a SyntheticAccountTypeListQuery object.');
+        });
+
+        it('Throws an error if "limit" query parameter is invalid', () => {
+            const promise = rizeClient.customer.getList({limit: ' '});
+            return expect(promise).to.eventually.be.rejectedWith('"limit" query must be an integer.');
+        });
+
+        it('Throws an error if "offset" query parameter is invalid', () => {
+            const promise = rizeClient.customer.getList({offset: ' '});
+            return expect(promise).to.eventually.be.rejectedWith('"offset" query must be an integer.');
+        });
+
+        it('Retrieves the synthetic account type list with query', async () => {
+            const query = {
+                limit: 2,
+                offset: 1,
+            };
+            const syntheticAccountTypes = await rizeClient.syntheticAccount.getTypesList(query);
+            verifySyntheticAccountTypesList(syntheticAccountTypes, query.limit, query.offset);
+        });
     });
 });
