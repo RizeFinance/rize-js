d707dc3eea0f3a080a44ad8f7ac56ab6f65e124a
doydoydoy <colourd.eyes@gmail.com>
2021-02-25 15:33:18 +0800

    Update synthetic-account.spec.js

--- test/core/synthetic-account.spec.js
+++ test/core/synthetic-account.spec.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const utils = require('../../lib/utils');
+
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
 
@@ -13,8 +15,104 @@ const rizeClient = new Rize(
 );
 
 describe('Synthetic Account', () => {
-    it('Test', async () => {
-        const test = await rizeClient.syntheticAccount.getList();
-        expect(test).to.not.be.empty;
+    describe('getList', async () => {
+        it('Throws an error if "query" is invalid', () => {
+            const promise = rizeClient.syntheticAccount.getList('');
+            return expect(promise).to.eventually.be.rejectedWith('"query" must be a SyntheticAccountListQuery object.');
+        });
+
+        it('Throws an error if "customer_uid" query is not an array', () => {
+            const query = { customer_uid: '' };
+            const promise = rizeClient.syntheticAccount.getList(query);
+            return expect(promise).to.eventually.be.rejectedWith('"customer_uid" query must be an array of strings.');
+        });
+
+        it('Throws an error if "customer_uid" query is not an array of strings', () => {
+            const query = { customer_uid: ['customer_uid', null] };
+            const promise = rizeClient.syntheticAccount.getList(query);
+            return expect(promise).to.eventually.be.rejectedWith('"customer_uid" query must be an array of strings.');
+        });
+
+        it('Throws an error if "external_uid" query is not a string', () => {
+            const query = { external_uid: null };
+            const promise = rizeClient.syntheticAccount.getList(query);
+            return expect(promise).to.eventually.be.rejectedWith('"external_uid" query must be a string.');
+        });
+
+        it('Throws an error if "pool_uid" query is not an array', () => {
+            const query = { pool_uid: '' };
+            const promise = rizeClient.syntheticAccount.getList(query);
+            return expect(promise).to.eventually.be.rejectedWith('"pool_uid" query must be an array of strings.');
+        });
+
+        it('Throws an error if "pool_uid" query is not an array of strings', () => {
+            const query = { pool_uid: ['pool_uid', null] };
+            const promise = rizeClient.syntheticAccount.getList(query);
+            return expect(promise).to.eventually.be.rejectedWith('"pool_uid" query must be an array of strings.');
+        });
+
+        it('Throws an error if "limit" query is not an integer', () => {
+            const query = { limit: 1.5 };
+            const promise = rizeClient.syntheticAccount.getList(query);
+            return expect(promise).to.eventually.be.rejectedWith('"limit" query must be an integer.');
+        });
+
+        it('Throws an error if "offset" query is not an integer', () => {
+            const query = { offset: 1.5 };
+            const promise = rizeClient.syntheticAccount.getList(query);
+            return expect(promise).to.eventually.be.rejectedWith('"offset" query must be an integer.');
+        });
+
+        it('Throws an error if "synthetic_account_type_uid" query is not a string', () => {
+            const query = { synthetic_account_type_uid: null };
+            const promise = rizeClient.syntheticAccount.getList(query);
+            return expect(promise).to.eventually.be.rejectedWith('"synthetic_account_type_uid" query must be a string.');
+        });
+
+        it('Throws an error if "synthetic_account_category" query is not on the synthetic_account_category options', () => {
+            const query = { synthetic_account_category: 'test' };
+            const promise = rizeClient.syntheticAccount.getList(query);
+            return expect(promise).to.eventually.be.rejectedWith('"synthetic_account_category" query must be a string. Accepted values are: general | external | plaid_external');
+        });
+
+        it('Throws an error if "program_uid" query is not a string', () => {
+            const query = { program_uid: null };
+            const promise = rizeClient.syntheticAccount.getList(query);
+            return expect(promise).to.eventually.be.rejectedWith('"program_uid" query must be a string.');
+        });
+
+        it('Throws an error if "liability" query is not a boolean', () => {
+            const query = { liability: null };
+            const promise = rizeClient.syntheticAccount.getList(query);
+            return expect(promise).to.eventually.be.rejectedWith('"liability" query must be boolean.');
+        });
+
+        it('Throws an error if "sort" query is not on the sort options', () => {
+            const query = { sort: null };
+            const promise = rizeClient.syntheticAccount.getList(query);
+            return expect(promise).to.eventually.be.rejectedWith('"sort" query must be a string. Accepted values are: name_asc | name_desc | net_usd_balance_asc | net_usd_balance_desc | net_usd_pending_balance_asc | net_usd_pending_balance_desc | net_usd_available_balance_asc | net_usd_available_balance_desc');
+        });
+
+        it('Retrieves the synthetic account list without query', async () => {
+            const syntheticAccountList = await rizeClient.syntheticAccount.getList();
+            utils.test.expectRizeList(syntheticAccountList);
+        });
+
+        it('Retrieves the synthetic account list with query', async () => {
+            const query = {
+                customer_uid: ['customer_uid1', 'customer_uid2'],
+                external_uid: 'external_uid',
+                pool_uid: ['pool_uid1', 'pool_uid2'],
+                limit: 50,
+                offset: 0,
+                synthetic_account_type_uid: 'synthetic_account_type_uid',
+                synthetic_account_category: 'general',
+                program_uid: 'program_uid',
+                liability: true,
+                sort: 'name_asc'
+            };
+            const syntheticAccountList = await rizeClient.syntheticAccount.getList(query);
+            utils.test.expectRizeList(syntheticAccountList);
+        });
     });
 });
