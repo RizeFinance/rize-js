023006d5f0e71580cf7420230c2eca3e42c5fe56
Neljon Lusterio <neljon@elephantventures.com>
2021-03-11 17:37:38 +0800

    Added transaction.getSyntheticLineItemList

--- types/lib/core/transaction.d.ts
+++ types/lib/core/transaction.d.ts
@@ -18,15 +18,40 @@ declare class TransactionService {
      */
     protected _validateGetListQuery(query: any): void;
     /**
+     * @ignore @protected
+     * Validates query parameter object for getSyntheticLineItemList method.
+     * @param {} query - An object containing key value pair for filtering the results list.
+     */
+    protected _validateGetSyntheticLineItemListQuery(query: any): void;
+    /**
      * Retrieves a list of Transactions filtered by the given parameters.
      * @param {TransactionListQuery} [query] - An object containing key value pair for filtering the results list.
      * @returns {Promise<RizeList<Transaction>>} A promise that returns a Transaction List if resolved.
      */
     getList(query?: TransactionListQuery): Promise<RizeList<Transaction>>;
+    /**
+     * Retrieves a list of Synthetic Line Items filtered by the given parameters.
+     * @param {SyntheticLineItemListQuery} query - An object containing key value pair for filtering the results list.
+     * @returns {Promise<RizeList<SyntheticLineItem>>} A promise that returns a Synthetic Line Item List if resolved.
+     * @example
+     * const customer = await rize.transaction.getSyntheticLineItemList({
+     *     customer_uid: ['customer_uid1', 'customer_uid2'],
+     *     pool_uid: ['pool_uid1', 'pool_uid2'],
+     *     synthetic_account_uid: ['synthetic_account_uid1', 'synthetic_account_uid2']
+     *     limit: 50,
+     *     offset: 0,
+     *     transaction_uid: 'transaction_uid1',
+     *     status: 'settled',
+     *     sort: 'name_asc'
+     * });
+     */
+    getSyntheticLineItemList(query?: SyntheticLineItemListQuery): Promise<RizeList<SyntheticLineItem>>;
 }
 declare namespace TransactionService {
-    export { TransactionListQuery, Transaction, RizeList };
+    export { TransactionListQuery, Transaction, SyntheticLineItemListQuery, SyntheticLineItem, RizeList };
 }
 type TransactionListQuery = import('./typedefs/transaction.typedefs').TransactionListQuery;
 type RizeList<T> = import('./typedefs/common.typedefs').RizeList<T>;
 type Transaction = import('./typedefs/transaction.typedefs').Transaction;
+type SyntheticLineItemListQuery = import('./typedefs/transaction.typedefs').SyntheticLineItemListQuery;
+type SyntheticLineItem = import('./typedefs/transaction.typedefs').SyntheticLineItem;
