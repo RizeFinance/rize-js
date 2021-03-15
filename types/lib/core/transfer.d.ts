4d9c0982f8474ed5d93dfff57263cd0e469b4d3c
Neljon Lusterio <neljon@elephantventures.com>
2021-03-15 20:22:44 +0800

    Implemented transfer.init

--- types/lib/core/transfer.d.ts
+++ types/lib/core/transfer.d.ts
@@ -23,6 +23,7 @@ declare class TransferService {
      * @param {string} uid
      */
     protected _validateGetParams(uid: string): void;
+    _validateInitParams(externalUid: any, sourceSyntheticAccountUid: any, destinationSyntheticAccountUid: any, initiatingCustomerUid: any, usTransferAmount: any): void;
     /**
      * Retrieves a list of Tranfers filtered by the given parameters.
      * @param {TransferListQuery} query - An object containing key value pair for filtering the results list.
@@ -46,6 +47,34 @@ declare class TransferService {
      * @example const transfer = await rize.transfer.get(transferUid);
      */
     get(uid: string): Promise<Transfer>;
+    /**
+     * Attempt to initiate a Transfer between two Synthetic Accounts. Before the Transfer will be initiated,
+     * several checks will be performed to ensure there is sufficient balance in the source account and that
+     * the initiating Customer has all the necessary access to both Synthetic Accounts. Depending on the
+     * Synthetic Account Types involved and the Program configuration, a new Transfer could complete instantly
+     * or take as many as 6 business days.
+     *
+     * Note: Rize is working to support any Synthetic Account as the source and destination. Currently, the
+     * only Synthetic Accounts allowed in a Transfer request are:
+     *
+     * - two general liability Synthetic Accounts
+     * - Master Synthetic Accounts and external Synthetic Accounts
+     * @param {string} externalUid - A unique identifier Client supplies. It should be given when creating a new resource and must be unique within the resource type. If the same value is given, no new resource will be created.
+     * @param {string} sourceSyntheticAccountUid - Synthetic Account to pull asset from. Must be an active liability or external-type account. Cannot be equal to `destination_synthetic_account_uid`.
+     * @param {string} destinationSyntheticAccountUid - Synthetic Account where the asset should land. Must be an active liability or external-type account. Cannot be equal to `source_synthetic_account_uid`.
+     * @param {string} initiatingCustomerUid
+     * @param {string} usTransferAmount - The USD amount to transfer.
+     * @returns {Promise<Transfer>} A promise that returns a Transfer if resolved.
+     * @example
+     * const transfer = await rize.transfer.create(
+     *     'external_uid1',
+     *     'synthetic_account_uid1',
+     *     'synthetic_account_uid2',
+     *     'customer_uid1',
+     *     100
+     * );
+     */
+    init(externalUid: string, sourceSyntheticAccountUid: string, destinationSyntheticAccountUid: string, initiatingCustomerUid: string, usTransferAmount: string): Promise<Transfer>;
 }
 declare namespace TransferService {
     export { TransferListQuery, Transfer, RizeList };
