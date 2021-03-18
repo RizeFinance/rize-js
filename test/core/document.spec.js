a3ee918e6d94bb4bff3e5b6a82a33a8f39105d40
Stephel Maca <stephelgeorgemaca@gmail.com>
2021-03-18 22:00:31 +0800

    Added document view

--- test/core/document.spec.js
+++ test/core/document.spec.js
@@ -15,6 +15,7 @@ const rizeClient = new Rize(
 );
 
 describe('Document', () => {
+    let testDocument;
 
     describe('getList', async () => {
         it('Throws an error if "query" is invalid', () => {
@@ -85,6 +86,7 @@ describe('Document', () => {
 
         it('Retrieves the document list without query', async () => {
             const documentList = await rizeClient.document.getList();
+            testDocument = documentList.data[0];
             utils.expectRizeList(documentList);
         });
 
@@ -101,6 +103,23 @@ describe('Document', () => {
             };
             const documentList = await rizeClient.document.getList(query);
             utils.expectRizeList(documentList);
+        });
+    });
+
+    describe('view', () => {
+        it('Throws an error if "uid" is empty', () => {
+            const promise = rizeClient.document.view('', 'pdf');
+            return expect(promise).to.eventually.be.rejectedWith('Document "uid" is required.');
+        });
+
+        it('Throws an error if "extension" is not valid', async () => {
+            const promise = rizeClient.document.view(testDocument.uid, 'excel');
+            return expect(promise).to.eventually.be.rejectedWith('Accepted values in the "extension" query are: pdf | html | json');
+        });
+
+        it('Download document data successfully', async () => {
+            const promise = rizeClient.document.view(testDocument.uid, 'pdf');
+            return expect(promise).to.be.fulfilled;
         });
     });
 });
